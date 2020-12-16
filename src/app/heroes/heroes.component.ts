6c536041b89d49451bec523c30912d293bc8ee08
Hideki Kamakura <kakihk695@gmail.com>
2020-12-16 17:27:02 +0900

    feat(heros.component.ts):add hero,message service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +10,25 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeros();
+  }
   onSelect(hero: Hero): void {
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     this.selectedHero = hero;
+  }
+  getHeros(): void {
+    this.heroService
+      .getHeroes()
+      .pipe(take(1))
+      .subscribe((heros: Hero[]) => (this.heroes = heros));
   }
 }
