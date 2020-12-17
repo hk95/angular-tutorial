cdae44e465a8f8840e172240cb37bb6aa4479f4a
Hideki Kamakura <kakihk695@gmail.com>
2020-12-17 15:37:01 +0900

    feat(hero-detail.html,ts):add save

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,4 +33,11 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService
+      .updateHero(this.hero)
+      .pipe(take(1))
+      .subscribe(() => this.goBack());
+  }
 }
